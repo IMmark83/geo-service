db8e49cb2e1cda5448abfbdef25270e99527339f
MalevichIM <malevichim@mail.ru>
2023-11-30 00:11:06 +0300

    Creatingg Test

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -1,15 +1,18 @@
 package ru.netology.geo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
 import ru.netology.entity.Location;
+
+import static ru.netology.geo.GeoServiceImpl.MOSCOW_IP;
 
 
 class GeoServiceImplTest {
     @Test
     void testByIp() {
         GeoService geoService = new GeoServiceImpl();
-        Location actual = geoService.byIp("127.0.0.1");
-        Location expected = new Location(null, null, null, 0);
+        Location actual = geoService.byIp("172.");
+        Location expected = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
         Assertions.assertEquals(expected, actual);
 
     }
