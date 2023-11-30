2f0d667ab77eb46e5d2e2768ee296427a1f4f6df
MalevichIM <malevichim@mail.ru>
2023-11-30 08:52:58 +0300

    changing GeoServiceImplTest

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -1,19 +1,18 @@
 package ru.netology.geo;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 
-import static ru.netology.geo.GeoServiceImpl.MOSCOW_IP;
-
-
 class GeoServiceImplTest {
     @Test
     void testByIp() {
         GeoService geoService = new GeoServiceImpl();
+        Location expected = new Location(("Moscow"), Country.RUSSIA, null, 0);
         Location actual = geoService.byIp("172.");
-        Location expected = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected.getCity(), actual.getCity());
+        Assertions.assertEquals(expected.getCountry(), actual.getCountry());
 
     }
 
